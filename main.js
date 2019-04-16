9c14659b8e0149bd26197c152521b450f31791b7
Tony Sun <tonysun996@gmail.com>
2019-04-15 23:20:10 -0400

    fix bug where there is blank country

--- main.js
+++ main.js
@@ -19,14 +19,15 @@ d3.csv("movies.csv", function(d) {
     country_imdbscore = {};
     country_budget_count = {}
     for (var i = 0; i < data.length; i++) {
-        country_count[data[i].country] = 1 + (country_count[data[i].country] || 0);
-        // account for missing budgets
-        if (data[i].budget) {
-            country_budget_count[data[i].country] = 1 + (country_budget_count[data[i].country] || 0);
+        if (data[i].country){ // in case country doesn't have name
+            country_count[data[i].country] = 1 + (country_count[data[i].country] || 0);
+            // account for missing budgets
+            if (data[i].budget) {
+                country_budget_count[data[i].country] = 1 + (country_budget_count[data[i].country] || 0);
+            }
+            country_budget[data[i].country] = (country_budget[data[i].country] + data[i].budget || data[i].budget);
+            country_imdbscore[data[i].country] = (country_imdbscore[data[i].country] + data[i].imdb_score || data[i].imdb_score);
         }
-        country_budget[data[i].country] = (country_budget[data[i].country] + data[i].budget || data[i].budget);
-        country_imdbscore[data[i].country] = (country_imdbscore[data[i].country] + data[i].imdb_score || data[i].imdb_score);
-        
     }
     country_data = []
     for (country in country_count){
@@ -75,6 +76,7 @@ function setup() {
         option.text = ops[i];
         selectList.appendChild(option);
     }
+    // add a axis label 
     svg.append("text")
         .attr("x", 80)
         .attr("y", 13)
