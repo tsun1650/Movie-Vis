5dff479bfb033940e74e69a06fbf9a88343cc5f2
Tony Sun <tonysun996@gmail.com>
2019-04-16 00:27:20 -0400

    chart shows countries sorted by avg budget

--- main.js
+++ main.js
@@ -33,8 +33,8 @@ d3.csv("movies.csv", function(d) {
     for (country in country_count){
         country_data.push({
             "country" : country,
-            "count" : +country_count[country],
-            "avg_budget": +country_budget[country] /  country_budget_count[country],
+            "count" : (+country_count[country] || 0),
+            "avg_budget": (+country_budget[country] /  country_budget_count[country] || 0)  ,
             "avg_imdbscore": +country_imdbscore[country] /  country_count[country]
         });   
     }    
@@ -60,10 +60,17 @@ function setup() {
     
     d3.select(graph)
         .append('p')
+            .attr('id','buttonzone')
             .append('button')
                 .attr('id','filterbutton')
                 .style("border", "1px solid black")
-                .text('Filter Data');
+                .text('Filter Data')
+    d3.select("#buttonzone")
+        .append('button')
+            .attr('id','desbutton')
+            .style("border", "1px solid black")
+            .text('Sort Descending')
+    
 
     //create select options to show countries by
     var selectList = document.createElement("SELECT")
@@ -83,19 +90,23 @@ function setup() {
         .attr("class", "label")
         .text("Countries");
 
-//   svg.append("text")
-//     .attr('id','yaxisname')
-//     .attr("x", width-2)
-//     .attr("y", height-6)
-//     .attr("text-anchor", "end")
-//     .attr("class", "label")
-//     .text("Exports (Millions of Dollars)");  
+    svg.append("text")
+        .attr('id','xaxisname')
+        .attr("x", width-2)
+        .attr("y", height-6)
+        .attr("text-anchor", "end")
+        .attr("class", "label")
+        .text(selectList.options[selectList.selectedIndex].value);  
 }
 
 function build_scales() {
     xScale = d3.scaleLinear().range([0, width]);
     yScale = d3.scaleBand().rangeRound([0, height], 0.8);
     yAxis = d3.axisLeft(yScale);
+
+    // https://stackoverflow.com/questions/42337710/d3-bar-chart-sorting-not-working
+    country_data.sort((a, b) => d3.descending(a.avg_budget, b.avg_budget));
+    
     xScale.domain([0, d3.max(country_data, function(d) {
         return d.avg_budget;
     })]);
@@ -114,6 +125,7 @@ function updateScalesFromData() {
         .call(yAxis);   
     bars.append('g')
         .selectAll('.bar')
+       
         .data(country_data)
         .enter()
         .append('rect')
@@ -129,6 +141,39 @@ function updateScalesFromData() {
             return yScale.bandwidth()*.8;
         });
     
+  }
+  function updating() {
+    d3.select("#sortAsc").on("click", function() {
+        console.log('clicked button')
+        country_data.sort(function(a, b) {
+          return d3.descending(a.avg_budget, b.avg_budget)
+        })
+        yScale.domain(country_data.map(function(d) {
+          return d.country;
+        }));
+        // sort by asc
+        // https://bl.ocks.org/anonymous/bc5a9691a3417b403d4e8ade3297afa3/3a2434c1c2849e476791e581754ec27e055db4d6
+        svg.selectAll(".bar")
+            .transition()
+            .duration(500)
+            .attr("y", function(d, i) {
+                return yScale(d.country);
+            })
+      
+        svg.selectAll(".val-label")
+            .transition()
+            .duration(500)
+            .attr("y", function(d, i) {
+                return yScale(d.country)+ yScale.bandwidth() / 2;
+            })
+      
+        svg.selectAll(".bar-label")
+            .transition()
+            .duration(500)
+            .attr("transform", function(d, i) {
+            return "translate(" + (yScale(d.country) + yScale.bandwidth() / 2 - 8) + "," + (width + 15) + ")" + " rotate(45)"
+            })
+      })
   }
 
 // function start() {
@@ -177,5 +222,6 @@ function initialize() {
     setup();
     build_scales();
     updateScalesFromData(); 
+    updating();
     
 }
